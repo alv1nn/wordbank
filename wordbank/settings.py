f05c3760b200954a284e107993c931c9cadae8f5
HenryMehta <hjsmehta@gmail.com>
2021-04-03 09:04:17 +0100

    py38

--- wordbank/settings.py
+++ wordbank/settings.py
@@ -180,5 +180,5 @@ LOGGING = {
 
 TEST_RUNNER = 'django.test.runner.DiscoverRunner'
 
-BLOGGER_API_KEY = os.environ.get('BLOGGER_API_KEY','AIzaSyCrkDL-8eidwLVHeDjMutcFc78hbbnfY-w')
-BLOG_ID = os.environ.get('BLOGGER_API_KEY','4368769871770527749')
+BLOGGER_API_KEY = os.environ['BLOGGER_API_KEY']
+BLOG_ID = os.environ['BLOGGER_API_KEY']
