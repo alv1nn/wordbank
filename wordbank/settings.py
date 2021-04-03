c9a1614eb719bf3ac859cf3e48786695d6349cb2
HenryMehta <hjsmehta@gmail.com>
2021-04-03 10:49:27 +0100

    py38

--- wordbank/settings.py
+++ wordbank/settings.py
@@ -1,5 +1,24 @@
 # Django settings for wordbank project.
 import os
+import requests
+
+
+def get_ec2_instance_ip():
+    """
+    Try to obtain the IP address of the current EC2 instance in AWS
+    """
+    try:
+        ip = requests.get(
+          'http://169.254.169.254/latest/meta-data/local-ipv4',
+          timeout=0.01
+        ).text
+    except requests.exceptions.ConnectionError:
+        return None
+
+    return ip
+
+
+AWS_LOCAL_IP = get_ec2_instance_ip()
 
 SITE_DIR = (os.path.join(os.path.dirname(__file__), '..')).replace('\\', '/')
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -50,7 +69,8 @@ ALLOWED_HOSTS = [
     '54.149.152.155',
 	'localhost', 
     'wordbank.stanford.edu',
-    '.us-west-2.elasticbeanstalk.com'
+    '.us-west-2.elasticbeanstalk.com',
+    AWS_LOCAL_IP
 ]
 
 # Local time zone for this installation. Choices can be found here:
