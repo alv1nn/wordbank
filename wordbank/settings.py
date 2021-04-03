799f0aca56136d02bdeaac25f7a6e0190d2eadf0
HenryMehta <hjsmehta@gmail.com>
2021-04-03 11:31:59 +0100

    py38

--- wordbank/settings.py
+++ wordbank/settings.py
@@ -1,17 +1,20 @@
 # Django settings for wordbank project.
 import os
 
-#Get AWS Healthchecker
-import urllib
+#Get AWS HealthChecker IP
+import requests
 def get_ec2_instance_ip():
     """
     Try to obtain the IP address of the current EC2 instance in AWS
     """
     try:
-        response = urllib.request.urlopen('http://169.254.169.254/latest/meta-data/local-ipv4')
-        return response.read()
-    except :
+        ip = requests.get(
+            'http://169.254.169.254/latest/meta-data/local-ipv4',
+            timeout=0.01
+        ).text
+    except requests.exceptions.ConnectionError:
         return None
+
     return ip
 AWS_LOCAL_IP = get_ec2_instance_ip()
 
